5be92253e49d1fd6ea8a11922c18f5da9038f7e5
jeanmw <jean.weatherwax@gmail.com>
2015-10-21 12:14:00 -0700

    initial comit

---
+++ MergeSortTests.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Arrays;
+
+
+public class MergeSortTest {
+
+  public void Test1() {
+    int[] arr = {5, 6, 2, 1, 4, 9, 12};
+    MergeSort.mergeSort(arr);
+
+
+  }
+
+  public void Test2() {
+    int[] arr = {};
+    MergeSort.mergeSort(arr);
+
+  }
+
+
+}
